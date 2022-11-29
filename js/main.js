1e22ce2f32b4854f3fbad9e062f75c59df4a6607
AlessandroDiNardo <dinardoalessandro98@gmail.com>
2022-11-29 17:51:14 +0100

    aggiunto bonus per lo stato done

--- js/main.js
+++ js/main.js
@@ -14,14 +14,18 @@ createApp({
             if(this.newTask.length < 5 || this.newTask === '') {
                 this.error = true;
             }else{
-                this.tasks.unshift({text:this.newTask});
+                this.tasks.unshift({text: this.newTask, done: false});
                 this.error = false;
             }
             this.newTask = '';
         },
-        
+
         removeTask(index){
             this.tasks.splice(index, 1);
+        },
+
+        doneTask(index){
+            this.tasks[index].done = !this.tasks[index].done;
         }
     }
 }).mount('#app')
