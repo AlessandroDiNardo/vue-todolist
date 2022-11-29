c46a16d7a41bed5fbfcf627a77b06810593257e6
AlessandroDiNardo <dinardoalessandro98@gmail.com>
2022-11-29 17:20:33 +0100

    Esercizio completo

--- js/main.js
+++ js/main.js
@@ -6,25 +6,22 @@ createApp({
             logoImg: './img/todolist.png',
             error: false,
             newTask: '',
-            tasks: [
-                { 
-                    text: 'Fare i compiti', 
-                    done: false 
-                }, 
-                { 
-                    text: 'Fare la spesa', 
-                    done: true 
-                }, 
-                { 
-                    text: 'Fare il bucato', 
-                    done: false 
-                }
-            ]
+            tasks: []
         }
     },
     methods: {
         addTask(){
-            this.tasks.unshift({text:this.newTask});
+            if(this.newTask.length < 5 || this.newTask === '') {
+                this.error = true;
+            }else{
+                this.tasks.unshift({text:this.newTask});
+                this.error = false;
+            }
+            this.newTask = '';
+        },
+        
+        removeTask(index){
+            this.tasks.splice(index, 1);
         }
     }
 }).mount('#app')
